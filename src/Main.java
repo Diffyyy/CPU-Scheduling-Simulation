7e923eb946225f781ffbf21504ced685164c3ef7
james-patrick-kok <james_patrick_kok@dlsu.edu.ph>
2023-10-18 19:27:09 +0800

    added scanner and Process class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int X, Y, Z, processId, arrivalTime, burstTime, i;
+        ArrayList<Process> processList = new ArrayList<Process>();
+        Scanner sc = new Scanner(System.in);
+
+        X = sc.nextInt();
+        Y = sc.nextInt();
+        Z = sc.nextInt();
+
+        for(i = 0; i < Y; i++){
+            processId = sc.nextInt();
+            arrivalTime = sc.nextInt();
+            burstTime = sc.nextInt();
+            processList.add(new Process(processId, arrivalTime, burstTime));
+        }
+
+
+
+        //FOR DEBUGGING ONLY
+        for(i = 0; i < Y; i++){
+            System.out.println("Process " + processList.get(i).processId + " | Arrival Time: " + processList.get(i).arrivalTime + " | burstTime: " + processList.get(i).burstTime);
+        }
+
     }
 }
---
+++ src/Process.java
@@ -0,0 +1,13 @@
+public class Process {
+    int processId;
+    int arrivalTime;
+    int burstTime;
+
+    public Process(int processId, int arrivalTime, int burstTime){
+        this.processId = processId;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+    }
+
+
+}
