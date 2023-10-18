eb1a081e292470e74791cacc91c8163337487e1b
james-patrick-kok <james_patrick_kok@dlsu.edu.ph>
2023-10-18 19:53:09 +0800

    added more attributes in Process class

--- src/Process.java
+++ src/Process.java
@@ -1,13 +1,50 @@
+import java.util.ArrayList;
+
 public class Process {
-    int processId;
-    int arrivalTime;
-    int burstTime;
+    private int processId;
+    private int arrivalTime;
+    private int burstTime;
+    private int startTime;
+    private int endTime;
+    private int remainingTime;
+    private int waitingTime;
 
     public Process(int processId, int arrivalTime, int burstTime){
         this.processId = processId;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
+        this.startTime = 0;
+        this.endTime = 0;
+        this.waitingTime = 0;
+        this.remainingTime = 0;
     }
 
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public int getProcessId() {
+        return processId;
+    }
+
+    public int getStartTime(){
+        return startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public int getRemainingTime() {
+        return remainingTime;
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
 
 }
